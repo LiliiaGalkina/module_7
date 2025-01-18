fb097e2f8c888ec9cbd4d0658e4da67220acbaf2
Liliia Galkina <vvlili@yandex.ru>
2025-01-18 19:44:19 +0300

    task 7.7.4

---
+++ task_774.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const person = {
+	firstName: "Jhon",
+	lastName: "Smit"
+}
+
+function setFullName(fullName) {
+	this.fullName = fullName;
+}
+
+const setPersonFullName = setFullName.bind(person);
+
+setPersonFullName("John Smith");
+
+console.log(person.fullName);
